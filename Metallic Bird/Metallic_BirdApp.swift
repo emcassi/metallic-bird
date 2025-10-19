7b54b4f3a507625c8868b8d2f3437ec2ea1016fe
Alex Sigalos <alex.wayne.dev@gmail.com>
2025-10-19 01:38:12 -0700

    Initial Commit

---
+++ Metallic Bird/Metallic_BirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metallic_BirdApp.swift
+//  Metallic Bird
+//
+//  Created by Alex Sigalos on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Metallic_BirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
