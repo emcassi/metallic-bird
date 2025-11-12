0f6e593386632756cf819069231f4c3a037412bb
Alex Sigalos <alex.wayne.dev@gmail.com>
2025-11-11 23:26:31 -0700

    Changed minimum gap height to 475

--- Metallic Bird/Pipe.swift
+++ Metallic Bird/Pipe.swift
@@ -20,7 +20,7 @@ class Pipe: GameObject {
         let gapMinY: Float = 400
         let gapMaxY = Float(screenSize.height - 1400)
         let gapStart = Float.random(in: gapMinY ... gapMaxY)
-        let gapHeightMin: Float = 450
+        let gapHeightMin: Float = 475
         let gapHeightMax: Float = 550
         let gapHeight = Float.random(in: gapHeightMin ... gapHeightMax)
         let topPipeY = gapStart - size.y * scale
